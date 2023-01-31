793d3b2bbf8bfd5c0b9dbd9a8f18e9e1293ccb3a
Ondairos <ondairos@gmail.com>
2023-01-31 17:21:34 +0200

    created AnecdoteList component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
 import NewAnecdote from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 import { increaseVote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
-  //increase vote function using dispatch and reducer helper function increaseVote
-  const vote = (id) => {
-    dispatch(increaseVote(id))
-  }
 
-  //anecdotes order by votes
-  const orderedByVotesAnectdotes = anecdotes.sort((a, b) => b.votes - a.votes)
-  
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {orderedByVotesAnectdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <h2>create new</h2>
-      <NewAnecdote/>
+      <NewAnecdote />
     </div>
   )
 }
