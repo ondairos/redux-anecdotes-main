a9525064b4421699cfc11e745cdb90e387f6a267
Ondairos <ondairos@gmail.com>
2023-02-02 12:22:14 +0200

    refactored filterReducer for app

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,21 +1,41 @@
-const filterReducer = (state = 'ALL', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = 'ALL'
+
+// const filterReducer = (state = 'ALL', action) => {
+//     switch (action.type) {
+//         case 'SET_FILTER':
+//             return action.payload
+//         default:
+//             return state
+
+//     }
+// }
+
+
+// //action creator function for filterReducer usage
+// export const filterChange = (myFilter) => {
+//     return {
+//         type: 'SET_FILTER',
+//         payload: myFilter
+//     }
+// }
+
+
+const filterSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        filterChange(state, action) {
+            // ;;;
             return action.payload
-        default:
-            return state
+        }
     }
-}
-
-
-//action creator function for filterReducer usage
-export const filterChange = (myFilter) =>{
-    return {
-        type: 'SET_FILTER',
-        payload: myFilter
-    }
-}
+})
 
 
 
-export default filterReducer
+// export default filterReducer
+export default filterSlice.reducer
+export const { filterChange } = filterSlice.actions
